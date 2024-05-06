3f9e7f90b6220107c6082ff94b08c7d2f39a1f42
ILwolf96 <ILwolf96@gmail.com>
2024-05-06 03:23:41 +0300

    update
    sfx and basic music

---
+++ El Chupacabra/Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    [SerializeField] private NewPlayerController controller;
+    [SerializeField] AudioSource Music;
+    [SerializeField] AudioSource Collectible;
+    [SerializeField] AudioSource PlayerAudioSource;
+    [SerializeField] AudioSource Enemey;
+    //
+    [SerializeField] AudioClip Music_Welcome;
+    [SerializeField] AudioClip Music_GameOver;
+    [SerializeField] AudioClip Music_Deloop;
+    //
+    [SerializeField] AudioClip CollectedGem;
+    [SerializeField] AudioClip PlayerWalkingSound;
+    [SerializeField] AudioClip JumpSound;
+    [SerializeField] AudioClip fullYell;
+    [SerializeField] AudioClip Yell_P1;
+    [SerializeField] AudioClip Yell_P2;
+    //
+    [SerializeField] AudioClip Basic_GotHitSound;
+    [SerializeField] AudioClip Melee_GotHitSound;
+    [SerializeField] AudioClip SpinAttack_Hit;
+    [SerializeField] AudioClip SpinAttack_Miss;
+    [SerializeField] AudioClip Dash_Hit;
+    [SerializeField] AudioClip Dash_Miss;
+    //
+    [SerializeField] AudioClip SmackHitSound;
+    [SerializeField] AudioClip Enemy_RangedMiss;
+
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+  
+        if (controller.IsJumping)
+        { PlayerAudioSource.clip = JumpSound; PlayerAudioSource.Play(); }
+
+      
+        
+    }
+}
+/*
+
+   public bool IsWalking
+   public bool IsJumping
+   public bool IsDoubleJumping
+   public bool IsGrounded
+   public bool IsHangingMB
+   public bool LeavingMB
+   public bool IsHangingEdge
+   public bool IsSpinAttacking
+   public bool IsDashing
+*/
