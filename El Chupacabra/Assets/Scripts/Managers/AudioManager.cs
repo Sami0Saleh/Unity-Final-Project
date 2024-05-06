ff239d86ba014a8ef9d0126816e6b29dc6212ea8
ILwolf96 <ILwolf96@gmail.com>
2024-05-06 05:11:58 +0300

    added Comments for Music and SFX in the Script

--- El Chupacabra/Assets/Scripts/Managers/AudioManager.cs
+++ El Chupacabra/Assets/Scripts/Managers/AudioManager.cs
@@ -11,31 +11,32 @@ public class AudioManager : MonoBehaviour
     [SerializeField] AudioSource PlayerAudioSource;
     [SerializeField] AudioSource Enemey;
     //
-    [SerializeField] AudioClip Music_Welcome;
-    [SerializeField] AudioClip Music_GameOver;
-    [SerializeField] AudioClip Music_Deloop;
-    [SerializeField] AudioClip Music_Victory;
-    [SerializeField] AudioClip Music_Marichis_1;
-    [SerializeField] AudioClip Music_Marichis_2;
+    [SerializeField] AudioClip Music_Welcome; // Start Game Music
+    [SerializeField] AudioClip Music_GameOver; // Game over Music
+    [SerializeField] AudioClip Music_Deloop; // Main Basic Loop (if it's not too annoying, just play it in endless loop)
+    [SerializeField] AudioClip Music_Victory; // Victory
+    [SerializeField] AudioClip Music_Marichis_1; // Bonus Music, might be fitting for after game over music, or just in the main menu)
+    [SerializeField] AudioClip Music_Marichis_2; // Bonus Music, might be fitting for after game Victory music, or just in the main)
     //
-    [SerializeField] AudioClip CollectedGem;
-    [SerializeField] AudioClip PlayerWalkingSound;
-    [SerializeField] AudioClip JumpSound;
-    [SerializeField] AudioClip fullYell;
-    [SerializeField] AudioClip Yell_P1;
-    [SerializeField] AudioClip Yell_P2;
+    [SerializeField] AudioClip CollectedGem; // when gem was collected
+    [SerializeField] AudioClip PlayerWalkingSound; // sound of when player is walking
+    [SerializeField] AudioClip JumpSound; // jump sound
+    [SerializeField] AudioClip fullYell; // Bonus SFX, combanition of two yells
+    [SerializeField] AudioClip Yell_P1; // Bonus SFX, First part of the two yells
+    [SerializeField] AudioClip Yell_P2; // Bonus SFX, Second Part of the two yells
     //
-    [SerializeField] AudioClip Basic_GotHitSound;
-    [SerializeField] AudioClip Melee_GotHitSound;
-    [SerializeField] AudioClip SpinAttack_Hit;
-    [SerializeField] AudioClip SpinAttack_Miss;
-    [SerializeField] AudioClip Dash_Hit;
-    [SerializeField] AudioClip Dash_Miss;
+    [SerializeField] AudioClip Basic_GotHitSound; // When The player got hit, can be environment damage, or ranged damaged 
+    [SerializeField] AudioClip Melee_GotHitSound; // when the player Get hits by a melee.
     //
-    [SerializeField] AudioClip SmackHitSound;
-    [SerializeField] AudioClip Enemy_RangedMiss;
+    [SerializeField] AudioClip SpinAttack_Hit; // When the player hits something with the Spin Attack
+    [SerializeField] AudioClip SpinAttack_Miss; // When the player misses with the Spin Attack
+    [SerializeField] AudioClip Dash_Hit; // When the player hits something with the dash Attack
+    [SerializeField] AudioClip Dash_Miss; //When the player hits something with the Spin Attack
+    //
+    [SerializeField] AudioClip SmackHitSound; // Bonus SFX, Basic Hit Sound of Smack.
+    [SerializeField] AudioClip Enemy_RangedMiss; // When the enemy misses the player with the with Ranged Attack
 
- 
+
 
     // Update is called once per frame
     void Update()
